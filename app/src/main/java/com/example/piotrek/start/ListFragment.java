6305aa2611764263be614af5c3b82e325b1fae47
korepetycjeapi <korepetycje1>
2018-11-28 19:58:51 +0100

    Pierwsze

---
+++ app/src/main/java/com/example/piotrek/start/ListFragment.java
@@ -0,0 +1,24 @@
+package com.example.piotrek.start;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+
+    View view;
+    public ListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_list,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/piotrek/start/MapFragment.java
@@ -0,0 +1,56 @@
+package com.example.piotrek.start;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+
+
+    View view;
+    public MapFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_map,container,false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googlemap)
+    {
+        googleMap = googlemap;
+        LatLng pp = new LatLng(51.7687323,19.4569911);
+        MarkerOptions option = new MarkerOptions();
+        option.position(pp).title("Łódź");
+
+        googleMap.addMarker(option);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(pp));
+    }
+}
